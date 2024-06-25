63d9474a012d0ca3159c63afdfa388f5dee73e4d
Amit kumar <132892687+amitkumar952@users.noreply.github.com>
2024-06-25 22:33:55 +0530

    JavaScript
    Chai aur Code

--- Prototype.js
+++ Prototype.js
@@ -17,24 +17,46 @@ Array.prototype.heyHitesh = function(){
   console.log(`Hitesh says Hello`);
 }
 
-myHeros.heyHitesh();
+//myHeros.heyHitesh();
 //heroPower.heyHitesh();
 // heroPower.hitesh();
 //myHeros.hitesh();
 
 //inheritance
+const User = {
+  name: "Chai",
+  email: "chai@google.com"
+}
 
 const Teacher ={
   makeVideo: true
 }
 
 
-const TeacherSupport ={
+const TeachingSupport ={
   isAvailable: false
 
 }
 
 const TASupport ={
   makeAssignment: 'js assignment',
-  fullTime: true
+  fullTime: true,
+  __proto__: TeachingSupport
 }
+Teacher.__proto__ = User
+
+
+//modern syntax
+Object.setPrototypeOf(TeachingSupport, Teacher)
+
+
+let anotherUsername = "ChaiAurCode   "
+
+String.prototype.trueLength = function(){
+  console.log(`${this}`);
+  console.log(`${this.name}`);
+  console.log(`True length is: ${this.trim().length}`);
+}
+anotherUsername.trueLength()
+"hitesh".trueLength()
+"iceTea".trueLength()
